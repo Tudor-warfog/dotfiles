4af7fde8f3bbdb97fc97dd9813bcacca5b6f8ea1
Tudor <tudor@Tudors-MacBook-Pro-2.local>
2018-08-23 20:15:12 +0200

    brew.sh

---
+++ brew.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install command-line tools using Homebrew
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+# Install other useful binaries
+brew install git
+brew install node
+brew install nvm
+
+brew install cask
+
+# Remove outdated versions from the cellar
+brew cleanup
